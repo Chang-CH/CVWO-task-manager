09e20b153f410bec1db418afbb106b2d629288cf
Chang Chuan Hao <e0543479@u.nus.edu>
2020-12-18 13:26:01 +0800

    Added writeups, possible fix to href in navbar

--- task-app/src/components/Navbar.js
+++ task-app/src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import * as Bootstrap from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 
 class Navbar extends Component {
     render(){
@@ -8,9 +9,9 @@ class Navbar extends Component {
             <Bootstrap.Navbar expand="lg" className="navbar flex-centered" sticky="top">
             <Bootstrap.Navbar.Brand href="/" className="nav-title">{this.props.text}</Bootstrap.Navbar.Brand>
             <Bootstrap.NavDropdown title="&equiv;" id="nav-drop">
-                <Bootstrap.NavDropdown.Item href="/categories">Categories</Bootstrap.NavDropdown.Item>
-                <Bootstrap.NavDropdown.Item href="/tasks">Tasks</Bootstrap.NavDropdown.Item>
-                <Bootstrap.NavDropdown.Item href="/tasks/new">New task</Bootstrap.NavDropdown.Item>
+                <Link to={{pathname: "/categories"}}><Bootstrap.NavDropdown.Item>Categories</Bootstrap.NavDropdown.Item></Link>
+                <Link to={{pathname: "/tasks"}}><Bootstrap.NavDropdown.Item>Tasks</Bootstrap.NavDropdown.Item></Link>
+                <Link to={{pathname: "/tasks/new"}}><Bootstrap.NavDropdown.Item>New task</Bootstrap.NavDropdown.Item></Link>
             </Bootstrap.NavDropdown>
             </Bootstrap.Navbar>
             </div>
